86eb5710adac07dae7e4a09c7bb2fce4b8677bca
md gauhar <mdgauhar700@gmail.com>
2026-01-12 21:29:42 +0530

    php-updates

--- src/app/sections/contact/contact.component.ts
+++ src/app/sections/contact/contact.component.ts
@@ -2,10 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
 
+  sendMessage(event: any) {
+    event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    fetch('https://darkslateblue-crane-217273.hostingersite.com/sendMail.php', {
+      method: 'POST',
+      body: formData
+    })
+      .then(res => res.text())
+      .then(msg => {
+        alert(msg);
+        form.reset();
+      })
+      .catch(err => alert('Error sending message.'));
+  }
 }
